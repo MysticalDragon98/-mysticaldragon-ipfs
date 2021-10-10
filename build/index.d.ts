73fa60b44cdb9168232fc209aa306a6ba12d1810
MysticalDragon98 <mysticaldragon1998@gmail.com>
2021-10-10 11:26:41 -0500

    Initial files

---
+++ build/index.d.ts
@@ -0,0 +1,12 @@
+interface IPFSOptions {
+    dir: string;
+}
+export declare class IPFS {
+    private options;
+    ipfs: any;
+    constructor(options: IPFSOptions);
+    init(): Promise<void>;
+    add(data: any): Promise<any>;
+    get(hash: string): Promise<any>;
+}
+export {};
---
+++ index.ts
@@ -0,0 +1,36 @@
+const Ipfs = require("ipfs");
+
+interface IPFSOptions {
+    dir: string;
+}
+
+export class IPFS {
+
+    ipfs: any;
+
+    constructor (private options: IPFSOptions) {
+        
+    }
+
+    async init () {
+        this.ipfs = await Ipfs.create({ repo: this.options.dir });
+    }
+
+    async add (data: any) {
+        const { path } = await this.ipfs.add(JSON.stringify(data));
+
+        return path;
+    }
+
+    async get (hash: string) {
+        const stream = this.ipfs.cat(hash)
+        let data = ''
+
+        for await (const chunk of stream) {
+            data += chunk.toString()
+        }
+
+        return JSON.parse(data);
+    }
+
+}
